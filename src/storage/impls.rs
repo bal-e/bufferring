1ab0283c48f0c5ccf8f609f1d6daeec37bacf455
Arav K <243i8560cm0o379p@bal-e.org>
2023-06-10 17:39:32 +0200

    impl the 'Storage' traits for 'MaybeUninit<T>'

--- src/storage/impls.rs
+++ src/storage/impls.rs
@@ -1,3 +1,5 @@
+use core::mem::MaybeUninit;
+
 use super::{Storage, PartialStorage, IndirectPartialStorage, FullStorage};
 
 unsafe impl<T: Storage> Storage for &mut T {
@@ -45,5 +47,30 @@ unsafe impl<T: FullStorage> FullStorage for &mut T {
 
     fn get_mut(&mut self) -> &mut [Self::Item] {
         T::get_mut(*self)
+    }
+}
+
+unsafe impl<T: Storage> Storage for MaybeUninit<T> {
+    type Item = T::Item;
+    type Capacity = T::Capacity;
+
+    unsafe fn capacity(this: *const Self) -> Self::Capacity {
+        // SAFETY: The caller guarantees that only the exposed elements can be
+        // uninitialized, so we can extract the capacity safely.
+        T::capacity(this as *const T)
+    }
+}
+
+unsafe impl<T: PartialStorage> PartialStorage for MaybeUninit<T> {
+    unsafe fn raw_ptr(this: *const Self) -> *const [Self::Item] {
+        // SAFETY: The caller guarantees that only the exposed elements can be
+        // uninitialized, so we can extract the actual raw pointer safely.
+        T::raw_ptr(this as *const T)
+    }
+
+    unsafe fn raw_ptr_mut(this: *mut Self) -> *mut [Self::Item] {
+        // SAFETY: The caller guarantees that only the exposed elements can be
+        // uninitialized, so we can extract the actual raw pointer safely.
+        T::raw_ptr_mut(this as *mut T)
     }
 }
